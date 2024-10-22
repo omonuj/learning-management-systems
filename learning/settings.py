7b29e1d107922851b27ab7babc28470797f56e0b
jonah odoh <jonahodoh@gmail.com>
2024-10-22 11:20:39 +0000

    do hard things

--- learning/settings.py
+++ learning/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'learning-management-systems-4.onrender.com',
+    'learning-management-systems-5.onrender.com',
     'localhost',
     '127.0.0.1'
 ]
