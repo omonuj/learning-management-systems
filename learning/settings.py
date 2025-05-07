0c3582e0f51be8c8f5c00efbb84ef28f7b939afa
jonah odoh <jonahodoh@gmail.com>
2025-05-07 10:31:59 +0100

    do hard things

--- learning/settings.py
+++ learning/settings.py
@@ -37,7 +37,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [
+    'learning-management-systems-10.onrender.com',
+
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
